62a1efc8543c40a2413c7a615e3e2234779a3b6d
Geremias Rodrigues <geremiasds00@gmail.com>
2024-11-17 07:24:39 -0300

    fiz algumas edições simples

---
+++ index.js
@@ -0,0 +1,13 @@
+function instagram(){
+    let remauare = document.getElementsByName('instagram');
+
+    let xitaq = remauare[0];
+
+    xitaq.style.background = 'blue'
+
+    xitaq.innerHTML += `<h1>Bem vindo ao meu perfil</h1>`
+
+    console.log(xitaq)
+
+    
+}
