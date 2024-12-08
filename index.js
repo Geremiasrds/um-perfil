f50a32288cf187cc010b42b7d9dbbbff197ad2ba
Geremias Rodrigues <geremiasds00@gmail.com>
2024-12-08 04:06:42 -0300

    fiz alteraçoes de js

--- index.js
+++ index.js
@@ -1,52 +1,29 @@
-document.getElementById("gitHub").addEventListener('click', () =>{
-
-meuGitHub()
-estilos()
-})
-
-
-
 async function buscarDados() {
     const armazenarDados = await fetch('https://api.github.com/users/geremiasrds')
     return await armazenarDados.json()
 
 }
-
-
-function meuGitHub() {
-    let aplicarInfor = buscarDados().then(infor => {
-        let addInfor =
-        `
-        <div class="o-novo">
-        <img src="${infor.avatar_url}" alt="Foto de perfil"/>
-        <h2>${infor.name}</h2>
-        <strong>${infor.bio}</strong>
-        </div>
-        `
-        document.querySelector(".novo").innerHTML = addInfor
-        addRepos()
-    })
-}
-
-
 async function buscarRepos() {
     const armazenarDados = await fetch('https://api.github.com/users/geremiasrds/repos')
     return await armazenarDados.json()
 
 }
+document.getElementById("gitHub").addEventListener('click', () => {
+    let pegar = document.querySelectorAll(".perfil")
+    let buscar = pegar[0]
+    buscar.classList.add("novo")
 
-function addRepos(){
     buscarRepos().then(dadosRepos => {
         let meuRepos = ""
         dadosRepos.forEach(repos => {
-          meuRepos +=
-          `
+            meuRepos +=
+                `
           <li><a href="${repos.html_url}" target="_blank">${repos.name}</a></li>
-          `  
+          `
 
         })
-        document.querySelector(".novo").innerHTML += 
-        `
+        document.querySelector(".novo").innerHTML +=
+            `
         <div class="o-novo">
         <h2>REPOSITORIOS</h2>
         <ul>${meuRepos}</ul>
@@ -54,10 +31,19 @@ function addRepos(){
         `
 
     })
-}
+    let aplicarInfor = buscarDados().then(infor => {
+        let addInfor =
+            `
+        <div class="o-novo">
+        <img src="${infor.avatar_url}" alt="Foto de perfil"/>
+        <h2>${infor.name}</h2>
+        <strong>${infor.bio}</strong>
+        </div>
+        `
+        document.querySelector(".novo").innerHTML = addInfor
 
-function estilos(){
-    let pegar = document.getElementsByTagName("main")
-    let mostar = pegar [0]
-    mostar.style.display="none"
-}
+
+    })
+})
+
+
